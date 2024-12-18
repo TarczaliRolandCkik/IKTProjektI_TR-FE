6b39a5dbd47de23c3f5024bef9c8c7bdfd2980b1
Tarczali Roland <tarczali.roland@ckik.loc>
2024-12-18 10:24:43 +0100

    C feladat.

--- IKTProjektI_TR-FE.py
+++ IKTProjektI_TR-FE.py
@@ -23,3 +23,21 @@ if osszeg<0:
     print(f"{osszeg} hiányzik a pénztárcánkból")
 elif osszeg>0:
     print(f"{osszeg} van a pénz tárcánkban")
+
+# c
+
+db=0
+db2=0
+
+for i in range(len(t)):
+    if t[i]<0:
+        db+=1
+for i in range(len(t)):
+    if t[i]>0:
+        db2+=1
+if db>db2:
+    print("Többször vesztettünk")
+elif db2>db:
+    print("Többször nyertünk.")
+else:
+    print("Ugyananyiszor nyertünk mint vesztettünk.")
