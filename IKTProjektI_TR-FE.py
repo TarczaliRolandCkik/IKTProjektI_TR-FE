a2974dde579697b7d3b0387ae28ef8aa63cfe478
Tarczali Roland <tarczali.roland@ckik.loc>
2024-12-18 10:15:13 +0100

    B feladat

--- IKTProjektI_TR-FE.py
+++ IKTProjektI_TR-FE.py
@@ -1,1 +1,25 @@
-hello.
+# Tarczali Roland
+# Feke Erik
+# 10C
+# "Első IKT beadandó"
+import random
+
+t=[]
+n=int(input())
+
+for i in range(n):
+    t.append(random.randint(-10000,10000))
+# kiírás
+for i in range (len(t)):
+    print(t[i], end=" ")
+print()
+
+
+# b
+osszeg=0
+for i in range (len(t)):
+    osszeg+=t[i]
+if osszeg<0:
+    print(f"{osszeg} hiányzik a pénztárcánkból")
+elif osszeg>0:
+    print(f"{osszeg} van a pénz tárcánkban")
